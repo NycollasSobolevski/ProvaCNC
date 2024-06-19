b693d5ee9fa4fe5c97b3c116e5125ad7c37b9143
nycollassobolevski <sobolevski.academy@gmail.com>
2024-06-19 09:25:47 -0300

    ADD Favicon

--- prova-cnc/src/app/app.config.ts
+++ prova-cnc/src/app/app.config.ts
@@ -1,13 +1,15 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, enableProdMode, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HttpClientModule } from '@angular/common/http';
 
+enableProdMode()
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
+    provideRouter(routes),
     provideAnimationsAsync(),
     importProvidersFrom(HttpClientModule)
   ]
